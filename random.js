90a52b3064c5b8ca6fc363d60c6411ce1ad64e9c
Noah Bystrom <noah.bystrom@calbt.com>
2018-08-01 13:24:27 -0700

    Add a library of random functions

---
+++ random.js
@@ -0,0 +1,41 @@
+export const random = {
+  shuffle,
+  randint,
+  choice,
+  sample
+}
+
+function shuffle (array)
+{
+  for (var i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+function randint (start=0, end)
+{
+  let range = end - start;
+  let num = Math.floor(Math.random() * (range)) + start;
+  return num;
+}
+
+function choice (array)
+{
+  let index = randint(array.length - 1);
+  return array[index];
+}
+
+function sample (array, count)
+{
+  let sample = [];
+  for (var i = 0; i < count; i++) {
+    let index = randint(array.length - 1);
+    sample.push(array[index]);
+    array.splice(index, 1);
+  }
+  return sample;
+}
